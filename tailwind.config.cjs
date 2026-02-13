627c38109eee586a5329226ee03c8b3145f25cbc
Desiree <devwithdesiree@gmail.com>
2026-02-13 12:56:26 -0600

    change color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,12 @@ module.exports = {
     extend: {
       colors: {
         goodness: {
+          white:"FDFCF9",
+          gold: "#C9A227",         // accent
+          charcoal: "#2B2B2B",     // primary text
+          beige: "#F4F1EC",        // soft section bg
+          muted: "#6B6B6B",
+          //remove below colors if not needed
           cream: "#FFF7E9",
           caramel: "#D29B6E",
           chocolate: "#4B2E2B",
